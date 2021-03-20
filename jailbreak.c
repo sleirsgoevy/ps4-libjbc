058cdc664b7185bbed8f35b77dc451c04815f1c2
LightningMods <34066913+LightningMods@users.noreply.github.com>
2021-03-20 03:00:50 -0400

    Fixed Module load for non-auth

--- jailbreak.c
+++ jailbreak.c
@@ -76,13 +76,26 @@ int jbc_get_cred(struct jbc_cred* ans)
 int jbc_set_cred(const struct jbc_cred* ans)
 {
     uintptr_t td = jbc_krw_get_td();
-    uintptr_t proc = jbc_krw_read64(td+8, KERNEL_HEAP);
-    uintptr_t ucred = jbc_krw_read64(proc+0x40, KERNEL_HEAP);
-    uintptr_t fd = jbc_krw_read64(proc+0x48, KERNEL_HEAP);
-    if(ppcopyin(&ans->uid, 1+&ans->svuid, ucred+4)
-    || ppcopyin(&ans->rgid, 1+&ans->svgid, ucred+20)
-    || ppcopyin(&ans->prison, 1+&ans->prison, ucred+0x30)
-    || ppcopyin(&ans->cdir, 1+&ans->jdir, fd+0x10))
+    uintptr_t proc = jbc_krw_read64(td + 8, KERNEL_HEAP);
+    uintptr_t ucred = jbc_krw_read64(proc + 0x40, KERNEL_HEAP);
+    uintptr_t fd = jbc_krw_read64(proc + 0x48, KERNEL_HEAP);
+
+
+    if(ans->sceProcCap != NULL)
+        ppcopyin(&ans->sceProcCap, 1 + &ans->sceProcCap, (uintptr_t)(((char*)ucred) + 104));
+
+        if(ans->sonyCred != NULL)
+            ppcopyin(&ans->sonyCred, 1 + &ans->sonyCred, (uintptr_t)(((char*)ucred) + 96));
+             
+            if (ans->sceProcType != NULL)
+               ppcopyin(&ans->sceProcType, 1 + &ans->sceProcType, (uintptr_t)(((char*)ucred) + 88));
+            
+
+
+   if (ppcopyin(&ans->uid, 1 + &ans->svuid, ucred + 4)
+        || ppcopyin(&ans->rgid, 1 + &ans->svgid, ucred + 20)
+        || ppcopyin(&ans->prison, 1 + &ans->prison, ucred + 0x30)
+        || ppcopyin(&ans->cdir, 1 + &ans->jdir, fd + 0x10))
         return -1;
     return 0;
 }
@@ -90,13 +103,20 @@ int jbc_set_cred(const struct jbc_cred* ans)
 int jbc_jailbreak_cred(struct jbc_cred* ans)
 {
     uintptr_t prison0 = jbc_get_prison0();
-    if(!prison0)
+    if (!prison0)
         return -1;
     uintptr_t rootvnode = jbc_get_rootvnode();
-    if(!rootvnode)
+    if (!rootvnode)
         return -1;
+
+    //without some modules wont load like Apputils
+    ans->sceProcCap = 0xffffffffffffffff;
+    ans->sceProcType = 0x3801000000000013;
+    ans->sonyCred = 0xffffffffffffffff;
+
     ans->uid = ans->ruid = ans->svuid = ans->rgid = ans->svgid = 0;
     ans->prison = prison0;
     ans->cdir = ans->rdir = ans->jdir = rootvnode;
     return 0;
 }
+
